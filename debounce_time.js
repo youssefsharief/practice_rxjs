cfb8a273926b7affce79f0c1a4bebfa7a858c787
Youssef Sherif <sharief@aucegypt.edu>
2018-04-18 14:34:07 +0200

    Add debounce time

--- debounce_time.js
+++ debounce_time.js
@@ -1,12 +1,12 @@
 const Rx = require('rxjs')
 
 //emit value every 1 second
-const source = Rx.Observable.interval(100);
+const source = Rx.Observable.interval(200).take(6);
 //incrementally increase the time to resolve based on source
-const promise = val =>
+const promise = () =>
   new Promise(resolve =>
-    setTimeout(() => resolve(`Resolved: ${val}`), 1000)
+    setTimeout(() => resolve(`New data`), 100)
   );
 //when promise resolves emit item from source
-source.throttle(promise)
+source.debounceTime(1000)
   .map(val => `Throttled off Promise: ${val}`).subscribe(val => console.log(val));
